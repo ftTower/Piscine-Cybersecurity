0219b37a9515a690dd52f71970f576c615cd3b82
ftTower <thomas.auer67@gmail.com>
2025-06-04 17:59:39 +0200

    Enhance spider.py to download images and add .gitignore for data directory

--- Arachnida/spider.py
+++ Arachnida/spider.py
@@ -2,19 +2,24 @@ import sys
 import requests
 
 from bs4 import BeautifulSoup
+from urllib.request import urlretrieve
+from urllib.parse import urlparse
+from urllib.parse import urljoin
 
 #! CHECKING ARGV
 if (len(sys.argv) < 2) :
     print("ERROR : missing url \n\tpython spider.py [-rlp] URL\n")
     sys.exit()
 
-#! GET URL WITH ARGV
+#! GET URL AND SITE NAME WITH ARGV
 URL = sys.argv[len(sys.argv) - 1]
+parsed_url = urlparse(URL)
+site_name = parsed_url.netloc
 
 #! GETTING HTML WITH URL
 try:
     page = requests.get(URL)
-    print(f"Current URL : {URL}\n")
+    # print(f"[+] working on {URL}\n")
 except requests.exceptions.RequestException as e:
     print(f"ERROR: Failed to fetch the URL.\n\nDetails:\n{e}")
     sys.exit()
@@ -22,12 +27,19 @@ except requests.exceptions.RequestException as e:
 #! SOUPIFY IT
 soup = BeautifulSoup(page.content, "html.parser")
 
-
 #! FIND IMG LINKS IN 
-result = soup.find_all("div")
-for site_img in result:
-    links = site_img.find_all("a")
-    for link in links:
-        link_url = link["href"]
-        print(f"Image here: {link_url}")
+result = soup.find_all("img")
+nb_img = 0
+for img in result:
+    if "src" in img.attrs:
+        img_url = img["src"]
+        full_img_url = urljoin(URL, img_url)
+        nb_img += 1
+        name_img = ".data/" + site_name + "_"  + str(nb_img) + ".png"
+        try:
+            urlretrieve(full_img_url, name_img)
+        except Exception as e:
+            print(f"Erreur lors du téléchargement de {full_img_url}: {e}")
+
+print(f"Found {nb_img} images on {URL}")
 
