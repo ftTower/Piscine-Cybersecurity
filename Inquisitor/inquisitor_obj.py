f4fd42d74541b3cbc03655306fcba0ad8fefca90
ftTower <thomas.auer67@gmail.com>
2025-07-20 04:23:58 +0200

    Refactor Inquisitor project: Move Machine class to a separate file for improved organization and maintainability

--- Inquisitor/inquisitor_obj.py
+++ Inquisitor/inquisitor_obj.py
@@ -1,28 +1,7 @@
+from machine_obj import *
+
 import sys
 from datetime import datetime
-
-class Machine:
-    def __init__(self, mac_address, ip_address, type):
-        self.ip_address = ip_address
-        self.mac_address = mac_address
-        self.type = type
-        self.poisoned = False
-    
-        if mac_address == None:
-           self.find_mac_adress() 
-    
-        
-    
-    def __str__(self):
-        return f"{self.type} : '{self.ip_address}' {self.mac_address} {self.poisoned}"
-    
-    def find_mac_adress(self):
-        pass
-    
-    def restore_arp_table(self):
-        pass
-
-
 
 
 def get_output_file_name():
---
+++ Inquisitor/machine_obj.py
@@ -0,0 +1,21 @@
+class Machine:
+    def __init__(self, mac_address, ip_address, type):
+        self.ip_address = ip_address
+        self.mac_address = mac_address
+        self.type = type
+        self.poisoned = False
+    
+        if mac_address == None:
+           self.find_mac_adress() 
+    
+        
+    
+    def __str__(self):
+        return f"{self.type} : '{self.ip_address}' {self.mac_address} {self.poisoned}"
+    
+    def find_mac_adress(self):
+        pass
+    
+    def restore_arp_table(self):
+        pass
+
