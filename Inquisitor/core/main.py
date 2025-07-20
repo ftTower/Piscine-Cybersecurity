dd51cb26b8030156cfffd97ca6f8deb14b9b6af4
ftTower <thomas.auer67@gmail.com>
2025-07-20 22:28:55 +0200

    switching makefile to script launcher

--- Inquisitor/core/main.py
+++ Inquisitor/core/main.py
@@ -1,24 +1,28 @@
 import sys
 from inquisitor_obj import *
 
-def main():
-	if len(sys.argv) != 5:
-		sys.exit(1)
+# def main():
+if len(sys.argv) != 5:
+	sys.exit(1)
 
-	try:
+# try:
 
-		inquisitor = Inquisitor(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
+inquisitor = Inquisitor(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
 
-		while True:
-			if not inquisitor.target.poisoned or not inquisitor.source.poisoned:
-				inquisitor.poisoned_arp_reply(inquisitor.looking_for_arp_requests())
-			else:
-				inquisitor.save_packets(inquisitor.capturing_packets())
+	# while True:
+		# if not inquisitor.target.poisoned or not inquisitor.source.poisoned:
+		# 	inquisitor.poisoned_arp_reply(inquisitor.looking_for_arp_requests())
+		# else:
+		# 	inquisitor.save_packets(inquisitor.capturing_packets())
+		# continue
 
-	except KeyboardInterrupt:
-		print("\nARP monitor stopped.")
-	except Exception as e:
-		print("\nFail in main loop.")
+# except KeyboardInterrupt:
+# 	print("\nARP monitor stopped.")
+# except Exception as e:
+# 	print(f"\nFail in main loop: {e}")
 
-if __name__ == "__main__":
-	main()
+# return 0
+
+# if __name__ == "__main__":
+# 	print("Starting ARP monitor...")
+# 	main()
