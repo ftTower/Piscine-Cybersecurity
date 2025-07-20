21c9943d7e2b89f88da5c66ef64319c9cd57e251
ftTower <thomas.auer67@gmail.com>
2025-07-20 22:34:53 +0200

    Refactor Inquisitor project: Update Makefile to include test arguments in run command, enhance main.py for improved error handling and functionality, and add ainsi.py for utility functions.

--- Inquisitor/core/main.py
+++ Inquisitor/core/main.py
@@ -1,28 +1,29 @@
 import sys
 from inquisitor_obj import *
+from ainsi import *
 
-# def main():
-if len(sys.argv) != 5:
-	sys.exit(1)
+def main():
+	if len(sys.argv) != 5:
+		sys.exit(1)
 
-# try:
+	try:
 
-inquisitor = Inquisitor(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
+		inquisitor = Inquisitor(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
 
-	# while True:
-		# if not inquisitor.target.poisoned or not inquisitor.source.poisoned:
-		# 	inquisitor.poisoned_arp_reply(inquisitor.looking_for_arp_requests())
-		# else:
-		# 	inquisitor.save_packets(inquisitor.capturing_packets())
-		# continue
+		while True:
+			if not inquisitor.target.poisoned or not inquisitor.source.poisoned:
+				inquisitor.poisoned_arp_reply(inquisitor.looking_for_arp_requests())
+			else:
+				inquisitor.save_packets(inquisitor.capturing_packets())
+			continue
 
-# except KeyboardInterrupt:
-# 	print("\nARP monitor stopped.")
-# except Exception as e:
-# 	print(f"\nFail in main loop: {e}")
+	except KeyboardInterrupt:
+		print(f"{ERASE_LINE}\nARP monitor stopped.")
+	except Exception as e:
+		print(f"{ERASE_LINE}\nFail in main loop: {e}")
 
-# return 0
+	return 0
 
-# if __name__ == "__main__":
-# 	print("Starting ARP monitor...")
-# 	main()
+if __name__ == "__main__":
+	print("Starting ARP monitor...")
+	main()
